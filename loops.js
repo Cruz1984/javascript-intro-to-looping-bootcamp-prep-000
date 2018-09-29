545cba41f1f19080d835e32f9d57e9a929f05ec2
MARY <mcruz2946@gmail.com>
2018-09-29 15:28:19 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,7 @@
+while (maybeTrue()) {console.log("and i ran; i ran so far away!");}
+let countdown = 100;
+while (countdown > 0){console.log(--countdown);}
+do {[loopBody];} while ([condition]);
+var 1 = 0;
+function incrementVariable() { i = i + 1;}
+do {console.log("doo-bee-doo-bee-doo");incrementVariable();} while (i < 5);
